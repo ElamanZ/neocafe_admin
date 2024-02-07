2ed17ef5b69d1fcd5f77895f58f8816ab3825924
ElamanZ <z.elaman699@gmail.com>
2024-02-08 01:55:38 +0600

    T4-41 Make authorization page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Routes from './routes/Routes.jsx';
 
 function App() {
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from './login.module.scss'
+import {useForm} from "react-hook-form";
 
+import eyeIcon from "../../assets/images/login/Eye.svg";
+import eyeIconNoVisible from "../../assets/images/login/Eye_NoVisible.svg";
+import login_picture from "../../assets/images/login/LoginPicture.png"
 function Login(props) {
+
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const inputType = showPassword ? 'text' : 'password';
+
+
     return (
-        <div>Login</div>
+        <>
+            <div className={styles.loginBlock}>
+                <div className={styles.login__content}>
+                    <div style={{width: '100%'}}>
+                        <div className={styles.content_title}>
+                            <span className={styles.black}>neo</span>
+                            <span className={styles.primary}>cafe</span>
+                        </div>
+                        <div className={styles.login}>
+                            <h1>Вход</h1>
+                            <input type="text" placeholder="Логин" className={styles.input_login}/>
+                            <div className={styles.login_passwordInput}>
+                                <input
+                                    type={inputType}
+                                    placeholder="Пароль"
+                                />
+                                <img
+                                    src={showPassword ? eyeIcon : eyeIconNoVisible}
+                                    alt="eyeIcon"
+                                    onClick={togglePasswordVisibility}
+                                />
+                            </div>
+                            <button className={styles.btn_login} disabled={true}>Войти</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.login__image}>
+                    <img src={login_picture} alt="login_picture"/>
+                </div>
+            </div>
+        </>
     );
 }
 
