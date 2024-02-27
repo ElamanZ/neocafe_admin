47414cdfe7dc080c050172f4aeaa9a9fc1466898
Venera <veneraomurzakkyzy@gmail.com>
2024-02-27 08:21:30 +0600

    Addlogin endpoint

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const instanse = axios.create({
+  baseURL: "https://neocafe-production.up.railway.app/",
+  headers: {
+    "Content-Type": "application.json",
+  },
+});
+
+export const login = async (data) => {
+  try {
+    const response = await instanse.post("/api/admin/auth", data);
+    console.log("Logged in");
+
+    return response.data;
+  } catch (error) {
+    console.log("Login failed");
+    throw error;
+  }
+};
